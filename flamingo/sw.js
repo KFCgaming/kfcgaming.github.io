89b638b4d0a556ebbaae30a5bb881e30cfb8e099
KFCgamingFR <53215372+KFCgaming@users.noreply.github.com>
2026-08-14 20:46:01 +0200

    Update sw.js

--- flamingo/sw.js
+++ flamingo/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "flamingo-v9";
+const CACHE = "flamingo-v10";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
